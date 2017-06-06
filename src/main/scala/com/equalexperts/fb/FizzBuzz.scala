eef6dbba96c07fe98e1c8cadb80d8c3241e9ec9f
j.pinto <joanna.pinto@net-a-porter.com>
2017-06-06 14:13:33 +0100

    Step 2

--- src/main/scala/com/equalexperts/fb/FizzBuzz.scala
+++ src/main/scala/com/equalexperts/fb/FizzBuzz.scala
@@ -1,11 +1,13 @@
+package com.equalexperts.fb
+
 case class FizzBuzz() {
 
   def generate(start: Int, end: Int): String = {
     if (start > end) s"Invalid range: $start is higher than $end"
-    else fizzBuzzList(start, end).mkString(" ")
+    else generateFizzBuzzList(start, end).mkString(" ")
   }
 
-  private def fizzBuzzList(start: Int, end: Int): List[String] = {
+  private def generateFizzBuzzList(start: Int, end: Int): List[String] = {
     (start to end map {
       case luckyNumber if luckyNumber.toString.contains("3") => "lucky"
       case nonLuckyNumber =>
